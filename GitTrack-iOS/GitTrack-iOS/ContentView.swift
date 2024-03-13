1d61b77865a7cc15128ada04f17dc9b7875d3230
jyk1029 <jyk102999@gmail.com>
2024-03-13 12:05:09 +0900

    set :: project setting

---
+++ GitTrack-iOS/GitTrack-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitTrack-iOS
+//
+//  Created by 김주영 on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTrack-iOS/GitTrack-iOS/GitTrack_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrack_iOSApp.swift
+//  GitTrack-iOS
+//
+//  Created by 김주영 on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTrack_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
