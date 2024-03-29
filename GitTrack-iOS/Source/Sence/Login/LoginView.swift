a934f90f7d2a11923d34fb657fc309d5c3c8927a
주영 <jyk102999@dsm.hs.kr>
2024-03-29 19:13:55 +0900

    add :: github login

---
+++ GitTrack-iOS/Source/Sence/Login/LoginView.swift
@@ -0,0 +1,45 @@
+//
+//  LoginView.swift
+//  GitTrack-iOS
+//
+//  Created by 김주영 on 3/26/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("GitTrack")
+                .font(.header1Bold)
+                .foregroundStyle(.blue500)
+            Text("Project, Git 분석을 위한")
+                .font(.body1Medium)
+                .foregroundStyle(.blue800)
+
+            Link(destination: URL(string: "http://localhost:8080/oauth2/authorization/github")!) {
+                HStack {
+                    Image(.githubLogo)
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Text("Github로 로그인하기")
+                        .font(.body3Medium)
+                        .foregroundStyle(.white)
+                }
+                .padding(.vertical, 16)
+                .padding(.horizontal, 55)
+            }
+            .background(.black)
+            .cornerRadius(16)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(.blue50).ignoresSafeArea())
+        .onOpenURL { url in
+            Token.accessToken = url.absoluteString.components(separatedBy: "code=").last ?? ""
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
